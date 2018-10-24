0d4e15bf2e843d3abafe873890d4745e7d7aaa89
Jason Lin <jasonlin43212@gmail.com>
2018-10-24 15:07:14 -0400

    seeing how it is adding to the file

--- fileTest.c
+++ fileTest.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main() {
-  int file_dest = open("test.txt", O_RDWR | O_APPEND);
+  int file_dest = open("test.txt", O_RDWR);
   printf("Opening the file <test.txt>\n");
   if (file_dest < 0) {
     printf("There was a problem opening the file.\n");
@@ -13,12 +13,12 @@ int main() {
   printf("File destination number : %d\n\n",file_dest);
   printf("Reading <test.txt>\n");
 
-  char text[400] = "";
-  if (read(file_dest,text,400) < 0) {
+  char text[600] = "";
+  if (read(file_dest,text,600) < 0) {
     printf("There was a problem reading the file.\n");
     return 1;
   }
-  printf("Contents of File:\n==================\n%s\n\n",text);
+  printf("Contents of File:\n==================\n%s\n",text);
 
 
   printf("Writing <This world is full of earthworms.> to the file\n\n");
@@ -28,22 +28,31 @@ int main() {
     return 1;
   }
 
-
-
-  printf("Rereading <test.txt>\n");
-
-  if (read(file_dest,text,10) < 0) {
-    printf("There was a problem reading the file.\n");
-    return 1;
-  }
-  printf("New contents of file:\n===========\n%s\n\n",text);
-
-
-  printf("Closing file\n");
+  printf("Closing file\n\n");
   if (close(file_dest) < 0){
     printf("There was a problem closing the file.\n");
     return 1;
   }
+
+  printf("Reopening file.\n\n");
+
+  int new_file_dest = open("test.txt", O_RDWR);
+
+  printf("Opening the file <test.txt>\n");
+  if (new_file_dest < 0) {
+    printf("There was a problem opening the file.\n");
+    return 1;
+  }
+  printf("File destination number : %d\n\n",new_file_dest);
+  printf("Reading <test.txt>\n");
+
+  char text2[600] = "";
+  if (read(new_file_dest,text2,600) < 0) {
+    printf("There was a problem reading the file.\n");
+    return 1;
+  }
+  printf("Contents of File:\n==================\n%s\n\n",text2);
+
 
   return 0;
 }
